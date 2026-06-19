6318db940c3ab4992095232334e764bbe672caec
SWEETY <sweetyyadav3369@gmail.com>
2026-06-19 20:19:16 +0530

    login process completed with making 1st cookie

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,8 @@
+const { v4: uuidv4 } = require('uuid')
 const User = require("../models/user");
+const { setUser } = require('../services/auth')
+
+
 
 async function handleUserSignup(req, res){
     const { name,email,password} = req.body;
@@ -7,9 +11,22 @@ async function handleUserSignup(req, res){
         email,
         password,
     });
-    return res.render("home");
+    return res.redirect("/");
 }
 
+async function handleUserLogin(req, res){
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password});
+    if(!user) return res.render("login", {
+        error: "Invalid Username or Password",
+    });
+
+    const sessionId = uuidv4();
+    setUser(sessionId, user);
+    res.cookie('uid', sessionId);
+    return res.redirect("/")
+}
 module.exports = {
     handleUserSignup,
+    handleUserLogin
 }
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -16,4 +16,7 @@ router.get('/signup', async(req,res) => {
     return res.render("signup")
 })
 
+router.get("/login", async(req, res) => {
+    return res.render("login")
+})
 module.exports = router;
